465b806e1c902c398e769d5db78dc49b4cc4b4f7
jack <jckhayes1@gmail.com>
2018-03-18 17:15:22 +1100

    Moved all none fabric functions to function file

---
+++ functions.py
@@ -0,0 +1,48 @@
+# Gets a date from a backups file name
+def get_date(filename):
+    split = filename.split('-')
+    year = split[1]
+    month = split[2]
+    day = split[3].split('.')[0]
+    return datetime(int(year),int(month),int(day))
+
+# Manages the backups of the site
+def manage_backups(name,local_dir,number_of_backups):
+    files = os.listdir(local_dir)
+    # The number of local backups exceed the limit
+    if len(files) > number_of_backups: 
+        # The oldest backup
+        oldest_backup = ""
+        oldest_backup_date = datetime.now()
+        # Loops through the files
+        for f in files:
+            # Compares the files date to the current oldest found date
+            if oldest_backup_date > get_date(f):
+                # Updates the oldest backup variables
+                oldest_backup_date = get_date(f)
+                oldest_backup = f
+
+        #Deletes the oldest backup
+        subprocess.call(['rm',local_dir + '/' + oldest_backup])
+
+#An error has been found
+def error_with_execution(backName):
+    errors.append(backName + '\n')
+
+# Signals the remote server to send the email
+def send_confirmation(results_string):
+    url = settings['confirm_backup_address']
+    payload = {'password': settings['confirm_backup_password'], 'email': settings['confirm_backup_email'],  'result': results_string}
+    r = requests.post(url, data=payload)
+
+# Checks the local backup device for remaining space
+def check_remaining_disk_space(local_dir):
+    # Gets the amount of space free on the local directory
+    command = ['df -Ph ' + local_dir + " | tail -1 | awk '{print $4}'"]
+    free = subprocess.check_output(command,shell=True).strip().decode('ascii')
+    # Gets the total amount of space availiable on the directory
+    command = ['df -Ph ' + local_dir + " | tail -1 | awk '{print $2}'"]
+    total = subprocess.check_output(command,shell=True).strip().decode('ascii')
+    # Constructs the message
+    message = "Directory " + local_dir + " has " + str(free) + "/" + str(total) + " remaining disk space"
+    return message
--- run_backup.py
+++ run_backup.py
@@ -2,6 +2,7 @@
 #pip install fabric
 from fabric.api import *
 from datetime import datetime
+from functions import *
 import json
 import requests
 import subprocess
@@ -85,33 +86,6 @@ def create_file_backup():
     except SystemExit:
         error_with_execution(name)
 
-# Gets a date from a backups file name
-def get_date(filename):
-    split = filename.split('-')
-    year = split[1]
-    month = split[2]
-    day = split[3].split('.')[0]
-    return datetime(int(year),int(month),int(day))
-
-# Manages the backups of the site
-def manage_backups(name,local_dir,number_of_backups):
-    files = os.listdir(local_dir)
-    # The number of local backups exceed the limit
-    if len(files) > number_of_backups: 
-        # The oldest backup
-        oldest_backup = ""
-        oldest_backup_date = datetime.now()
-        # Loops through the files
-        for f in files:
-            # Compares the files date to the current oldest found date
-            if oldest_backup_date > get_date(f):
-                # Updates the oldest backup variables
-                oldest_backup_date = get_date(f)
-                oldest_backup = f
-
-        #Deletes the oldest backup
-        subprocess.call(['rm',local_dir + '/' + oldest_backup])
-
 # The full list of hosts has been run through
 @runs_once
 def finished():
@@ -135,25 +109,3 @@ def finished():
                 results_string = results_string + message + "\n"
         #Sends the confirmation request
         send_confirmation(results_string)
-
-#An error has been found
-def error_with_execution(backName):
-    errors.append(backName + '\n')
-
-# Signals the remote server to send the email
-def send_confirmation(results_string):
-    url = settings['confirm_backup_address']
-    payload = {'password': settings['confirm_backup_password'], 'email': settings['confirm_backup_email'],  'result': results_string}
-    r = requests.post(url, data=payload)
-
-# Checks the local backup device for remaining space
-def check_remaining_disk_space(local_dir):
-    # Gets the amount of space free on the local directory
-    command = ['df -Ph ' + local_dir + " | tail -1 | awk '{print $4}'"]
-    free = subprocess.check_output(command,shell=True).strip().decode('ascii')
-    # Gets the total amount of space availiable on the directory
-    command = ['df -Ph ' + local_dir + " | tail -1 | awk '{print $2}'"]
-    total = subprocess.check_output(command,shell=True).strip().decode('ascii')
-    # Constructs the message
-    message = "Directory " + local_dir + " has " + str(free) + "/" + str(total) + " remaining disk space"
-    return message
